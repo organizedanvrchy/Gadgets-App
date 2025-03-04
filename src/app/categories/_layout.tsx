fe5def69438f755565b844605a5cd0c4e4e22443
organizedanvrchy <vimalramnarain@gmail.com>
2025-03-04 00:58:38 -0500

    Updated Categories Layout

--- src/app/categories/_layout.tsx
+++ src/app/categories/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+
 export default function CategoriesLayout() {
   return (
     <Stack>
