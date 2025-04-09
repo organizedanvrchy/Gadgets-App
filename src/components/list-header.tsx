78deab06e3e0932b5ca208e63f5c3f044e1c0377
organizedanvrchy <vimalramnarain@gmail.com>
2025-04-09 16:06:22 -0400

    Updated list-header file and added +not-found file to handle any 404 links

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router';
-import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
+import { CATEGORIES } from '../../assets/categories';
 
 export const ListHeader = () => {
   return (
@@ -45,7 +46,26 @@ export const ListHeader = () => {
           style={ styles.heroImage }
         />
       </View>
-      <View style={ styles.categoriesContainer}></View>
+      <View style={ styles.categoriesContainer}>
+        <Text style={ styles.sectionTitle }>Categories</Text>
+        <FlatList
+          data={ CATEGORIES }
+          renderItem={({ item }) => (
+            <Link asChild href={`/categories/${ item.slug }`}>
+              <Pressable style={ styles.category }>
+                <Image
+                  source={{ uri: item.imageUrl }}
+                  style={ styles.categoryImage }
+                />
+                <Text style={ styles.categoryText }>{ item.name }</Text>
+              </Pressable>
+            </Link>
+          )}
+          keyExtractor={ item => item.name }
+          horizontal
+          showsHorizontalScrollIndicator={ false }
+        />
+      </View>
     </View>
   );
 };
